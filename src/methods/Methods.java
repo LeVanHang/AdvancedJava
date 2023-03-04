bca24014a21f65dfb7086fc033ced8b6dcdfac61
LeVanHang <vanhang1802@gmail.com>
2023-03-05 03:28:43 +0700

    first commit

---
+++ src/methods/Methods.java
@@ -0,0 +1,23 @@
+package methods;
+
+public class Methods {
+    /*
+    Phương thức là một khối mã chỉ chạy khi nó được gọi.
+
+Bạn có thể chuyển dữ liệu, được gọi là tham số, vào một phương thức.
+
+Các phương thức được sử dụng để thực hiện một số hành động nhất định và chúng còn được gọi là chức năng.
+
+Tại sao sử dụng phương pháp? Để sử dụng lại mã: xác định mã một lần và sử dụng nó nhiều lần.
+
+Tạo một phương thức
+Một phương thức phải được khai báo trong một lớp. Nó được xác định với tên của phương thức,
+ tiếp theo là dấu ngoặc đơn (). Java cung cấp một số phương thức được xác định trước, chẳng hạn như ,
+ nhưng bạn cũng có thể tạo các phương thức của riêng mình để thực hiện một số hành động nhất định:System.out.println()
+
+ myMethod() là tên của phương thức
+static có nghĩa là phương pháp thuộc về Lớp chính và không phải là một đối tượng của lớp Chính.
+Bạn sẽ tìm hiểu thêm về các đối tượng và cách truy cập các phương thức thông qua các đối tượng ở phần sau của hướng dẫn này.
+void có nghĩa là phương pháp này không có giá trị trả về. Bạn sẽ tìm hiểu thêm về các giá trị trả về ở phần sau của chương này
+     */
+}
